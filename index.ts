c90b910751941721ee2d315dd83654911e9f11f3
Vilson Ferrari <vilsonferrarifreddi@gmail.com>
2021-03-09 19:32:23 -0300

    change api endpoints

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import Get from './api/get'
 import Save from './api/save'
 const port = app.get('port')
 
-app.get('/reports', Get)
-app.post('/report', Save)
+app.get('/get', Get)
+app.post('/save', Save)
 
 app.listen(port, () => {
     console.log(`Server is running at https://localhost:${port}`)
